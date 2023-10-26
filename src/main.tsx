cb025d4345045d86715f9d14b34cc2136440a525
Rahul Menon <rahul@Rahuls-MacBook-Pro.local>
2023-10-26 22:29:47 +0530

    test cw analytics

--- src/main.tsx
+++ src/main.tsx
@@ -89,7 +89,7 @@ function AppContainer() {
       // })}
       activeChain="optimism-goerli"
       key={wallets.map((w) => w.id).join(",") + enableSmartWallet}
-      clientId="b3b2f0559e859ef264eadf2fae39182c"
+      clientId={process.env.CLIENT_ID,}
       supportedWallets={walletConfigs}
       supportedChains={[...defaultChains, FncyTestnet]}
     >
